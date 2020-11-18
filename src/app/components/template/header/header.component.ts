c51393de86e8688ad60320500324a47e5f279e2a
Carlos <carloscms2009@gmail.com>
2020-11-18 00:05:01 -0300

    .

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var require: any
-
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  private headerLogo = require('../../../../assets/imgs/suchSoftwareNoBg.png')
 
   constructor() { }
 
